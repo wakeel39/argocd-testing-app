2833f0ebdb3254bc9d80142d0152c1efc9d4b525
sardar <sardar.abdulwakeel@feedus.io>
2026-02-27 01:41:42 +0400

    -- udpate deploy latest verison update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello Sardar you updated the app 2222 222 333')
+  res.send('Hello Sardar you updated the app verison update ')
 })
 
 app.listen(3000, () => console.log('Running on 3000'))
